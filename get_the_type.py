a0312b6f206efbae055d8eb7afa20088c592c103
nagesha technology <asbconstruction19@gmail.com>
2025-01-13 12:31:56 +0530

    get the data types, Many Values to Multiple Variables

---
+++ get_the_type.py
@@ -0,0 +1,32 @@
+#can get the data type of a variable
+x = 5
+y = "john"
+print(type(x))
+print(type(y))
+"""
+<class 'int'>
+<class 'str'>
+"""
+
+
+#Many Values to Multiple Variables
+x , y, z = "Orange", "Banana", "Apple"
+print(x)
+print(y)
+print(z)
+"""
+Orange
+Banana
+Apple
+"""
+
+#One Value to Multiple Variables
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+"""
+orange
+orange
+orange
+"""
