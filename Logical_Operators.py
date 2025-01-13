71872eaf346a9ccc06dce8163c5700cc9ee81010
nagesha technology <asbconstruction19@gmail.com>
2025-01-13 13:27:31 +0530

    Logical_Operators

---
+++ Logical_Operators.py
@@ -0,0 +1,25 @@
+# using and
+# x = 3
+# print(x > 1 and x < 5)
+# #output: True
+# print(x > 2 and x > 5)
+# output: False
+#
+# using or
+# x = 4
+# print (x > 1 or x < 2)
+# output: True
+# print(x > 4 or x < 2)
+# output: False
+#
+# using not
+# x = 3
+# print(not(x >1 and x < 5))
+# output: False
+
+#Combining All Operators
+x = 4
+y = 6
+result = (x > 2 and y < 10) or not(x < 3)
+print(result)
+#output: true
