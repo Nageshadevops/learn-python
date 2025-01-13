ecdf5901183c9e2f0649fdacdc33139ac1433fe0
nagesha technology <asbconstruction19@gmail.com>
2025-01-13 15:36:49 +0530

    Membership Operators

---
+++ operator_Precedence.py
@@ -0,0 +1,18 @@
+'''
+#1. Parentheses
+result = (2 + 3) * 4
+print(result)
+#output: 20
+'''
+
+"""
+#2. Exponentiation (**)
+result = 2 ** 3 ** 2
+print(result)
+#output: 512
+"""
+
+#3. Unary Operators (+x, -x, ~x)
+result = -3 +5
+print(result)
+#output: 2
