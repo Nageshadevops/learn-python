717eab9d406430ff0b12920ddb79437ce39afc09
nagesha technology <asbconstruction19@gmail.com>
2025-01-13 13:59:50 +0530

    Membership Operators

---
+++ membership_operators.py
@@ -0,0 +1,15 @@
+#Membership Operators
+"""
+#using in
+fruits = ["apple", "orange", "grapes"]
+print("apple" in fruits)
+#output:True
+print("banana" in fruits)
+#output:False
+"""
+
+fruits = ["apple", "orange", "grapes"]
+print("banana" not in fruits)
+#output:True
+print("apple" not in fruits)
+#output:False
